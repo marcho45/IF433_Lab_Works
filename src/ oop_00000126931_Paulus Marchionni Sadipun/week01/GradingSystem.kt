df012d02fe59895e064b71cb763eacfdda488e50
marcho45 <paulusmarchionni@gmail.com>
2026-02-09 22:30:24 +0700

    week01: add calculateStatus function

--- src/ oop_00000126931_Paulus Marchionni Sadipun/week01/GradingSystem.kt
+++ src/ oop_00000126931_Paulus Marchionni Sadipun/week01/GradingSystem.kt
@@ -1,5 +1,6 @@
 package ` oop_00000126931_Paulus Marchionni Sadipun`.week01
 
+
 fun main() {
     val name = "John Thor"
     val score = 80
@@ -7,12 +8,23 @@ fun main() {
     // String Template
     println("Nama: $name, Nilai: $score")
 
+    // When Expression
+    val grade = when (score) {
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "D"
+    }
+    println("Grade kamu: $grade")
+
+    // Function call
+    println("Status: ${calculateStatus(score)}")
+
+    // Null Safety
+    val studentId: String? = null
+    val idLength = studentId?.length ?: 0 // Elvis Operator
+    println("Panjang ID: $idLength")
 }
 
-val grade = when (score) {
-    in 90..100 -> "A"
-    in 80..89 -> "B"
-    in 70..79 -> "C"
-    else -> "D"
-}
-println("Grade kamu: $grade")
+// Expression Body Function
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
