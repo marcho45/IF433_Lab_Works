6dd29103ce16d88a61fd96b8181b2a2b515487b7
marcho45 <paulusmarchionni@gmail.com>
2026-02-09 23:22:26 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000126931_PaulusMarchionniSadipun/week01/CircleCalculator.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week01/CircleCalculator.kt
@@ -1,17 +1,12 @@
 package oop_00000126931_paulus_marchionni_sadipun.week01
 
-fun main(args: Array<String>) {
+fun main() {
     val radius = 7.0
     val pi = 3.14
     val area = pi * radius * radius
 
     println("Radius: $radius, Area: $area")
-
-    // REFACTOR: Panggil fungsi di dalam println
-    // Karena fungsi checkSize sekarang mengembalikan String (teks), bukan langsung print.
     println(checkSize(area))
 }
 
-// REFACTOR: Ubah menjadi Expression Body Function (=)
-// Menggunakan if sebagai expression yang mengembalikan nilai String
 fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
