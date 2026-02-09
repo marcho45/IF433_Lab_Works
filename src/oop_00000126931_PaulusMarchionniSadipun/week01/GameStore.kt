b0ff052a308144dd5fbc9ee741680437967da8cd
marcho45 <paulusmarchionni@gmail.com>
2026-02-09 23:36:16 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000126931_PaulusMarchionniSadipun/week01/GameStore.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week01/GameStore.kt
@@ -4,11 +4,18 @@ fun main() {
     val gameTitle = "Elden Ring"
     val price = 600000
 
-    // Panggil fungsi diskon (untuk mengetes)
     val discount = calculateDiscount(price)
-    println("Diskon: $discount")
+    val finalPrice = price - discount
+
+    // Langkah 3: Panggil dengan Named Arguments
+    printReceipt(title = gameTitle, finalPrice = finalPrice)
 }
 
-// Langkah 2: Logika Diskon (Expression Body)
-// Jika harga > 500.000 diskon 20%, selain itu 10%
 fun calculateDiscount(price: Int) = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
+
+// Fungsi Cetak Struk
+fun printReceipt(title: String, finalPrice: Int) {
+    println("=== STRUK PEMBELIAN ===")
+    println("Judul Game : $title")
+    println("Harga Akhir: Rp $finalPrice")
+}
