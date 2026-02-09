9142a0944997cc191fe997378009cc4a5a738105
marcho45 <paulusmarchionni@gmail.com>
2026-02-09 22:41:43 +0700

    week01: initial setup and procedural code

--- src/ oop_00000126931_Paulus Marchionni Sadipun/week01/GradingSystem.kt
+++ src/ oop_00000126931_Paulus Marchionni Sadipun/week01/GradingSystem.kt
@@ -1,18 +1,3 @@
 package ` oop_00000126931_Paulus Marchionni Sadipun`.week01
 
 
-fun main() {
-    val name = "John Thor"
-    val score = 80
-
-    // String Template
-    println("Nama: $name, Nilai: $score")
-
-    val grade = when (score) {
-        in 90..100 -> "A"
-        in 80..89 -> "B"
-        in 70..79 -> "C"
-        else -> "D"
-    }
-
-}
