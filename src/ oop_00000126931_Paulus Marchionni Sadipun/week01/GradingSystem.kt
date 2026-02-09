749df1757981820cc8df7904a8404ecfd8582843
marcho45 <paulusmarchionni@gmail.com>
2026-02-09 22:43:05 +0700

    week01: initial setup and procedural code

--- src/ oop_00000126931_Paulus Marchionni Sadipun/week01/GradingSystem.kt
+++ src/ oop_00000126931_Paulus Marchionni Sadipun/week01/GradingSystem.kt
@@ -1,3 +1,10 @@
 package ` oop_00000126931_Paulus Marchionni Sadipun`.week01
 
 
+fun main() {
+    var name = "John Thor"
+    var score = 80
+
+    // String Template
+    println("Nama: $name, Nilai: $score")
+}
