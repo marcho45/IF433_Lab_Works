68d8a95a98bd3570aa1c82aa0cd85f7aede7d212
marcho45 <paulusmarchionni@gmail.com>
2026-02-09 22:38:10 +0700

    week01: initial setup and procedural code

--- src/ oop_00000126931_Paulus Marchionni Sadipun/week01/GradingSystem.kt
+++ src/ oop_00000126931_Paulus Marchionni Sadipun/week01/GradingSystem.kt
@@ -2,29 +2,10 @@ package ` oop_00000126931_Paulus Marchionni Sadipun`.week01
 
 
 fun main() {
-    val name = "John Thor"
-    val score = 80
+    var name = "John Thor"
+    var score = 80
 
     // String Template
     println("Nama: $name, Nilai: $score")
 
-    // When Expression
-    val grade = when (score) {
-        in 90..100 -> "A"
-        in 80..89 -> "B"
-        in 70..79 -> "C"
-        else -> "D"
-    }
-    println("Grade kamu: $grade")
-
-    // Function call
-    println("Status: ${calculateStatus(score)}")
-
-    // Null Safety
-    val studentId: String? = null
-    val idLength = studentId?.length ?: 0 // Elvis Operator
-    println("Panjang ID: $idLength")
 }
-
-// Expression Body Function
-fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
