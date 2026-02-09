eb32c287bb4de63c620f215e7fcf146c26673c43
marcho45 <paulusmarchionni@gmail.com>
2026-02-09 22:39:28 +0700

    week01: implement grading logic using when expression

--- src/ oop_00000126931_Paulus Marchionni Sadipun/week01/GradingSystem.kt
+++ src/ oop_00000126931_Paulus Marchionni Sadipun/week01/GradingSystem.kt
@@ -8,4 +8,11 @@ fun main() {
     // String Template
     println("Nama: $name, Nilai: $score")
 
+    val grade = when (score) {
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "D"
+    }
+
 }
