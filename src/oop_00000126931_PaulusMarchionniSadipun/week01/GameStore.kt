3612724b81389d4a54c8daebec26b3eb5cd513a9
marcho45 <paulusmarchionni@gmail.com>
2026-02-09 23:36:03 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000126931_PaulusMarchionniSadipun/week01/GameStore.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week01/GameStore.kt
@@ -1,7 +1,14 @@
 package oop_00000126931_paulus_marchionni_sadipun.week01
 
 fun main() {
-    // Langkah 1: Setup Data menggunakan val
     val gameTitle = "Elden Ring"
     val price = 600000
+
+    // Panggil fungsi diskon (untuk mengetes)
+    val discount = calculateDiscount(price)
+    println("Diskon: $discount")
 }
+
+// Langkah 2: Logika Diskon (Expression Body)
+// Jika harga > 500.000 diskon 20%, selain itu 10%
+fun calculateDiscount(price: Int) = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
