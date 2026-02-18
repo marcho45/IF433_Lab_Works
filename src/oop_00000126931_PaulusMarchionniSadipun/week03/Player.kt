e2f221e5c3f2a41d0c56c6e0e7d38294f24640d4
marcho45 <paulusmarchionni@gmail.com>
2026-02-18 16:05:18 +0700

    week03: (task 2) create Player class with private xp

---
+++ src/oop_00000126931_PaulusMarchionniSadipun/week03/Player.kt
@@ -0,0 +1,26 @@
+package oop_00000126931_PaulusMarchionniSadipun.week03
+
+class Player(val username: String) {
+    private var xp: Int = 0
+
+
+    val level: Int
+        get() = (xp / 100) + 1
+
+
+    fun addXp(amount: Int) {
+        if (amount > 0) {
+            val oldLevel = level
+
+            xp += amount
+            println("$username mendapatkan $amount XP.")
+
+
+            if (level > oldLevel) {
+                println("Level Up! Selamat $username naik ke level $level")
+            }
+        } else {
+            println("Gagal: XP harus bernilai positif!")
+        }
+    }
+}
