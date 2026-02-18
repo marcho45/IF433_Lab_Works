9cd0488626507cc95bd83985e1c7b3959011a6a5
marcho45 <paulusmarchionni@gmail.com>
2026-02-18 10:56:16 +0700

    week02: (task 1) implement library input system

--- src/oop_00000126931_PaulusMarchionniSadipun/week02/Main.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week02/Main.kt
@@ -1,43 +1,34 @@
 package oop_00000126931_PaulusMarchionniSadipun.week02
 
 import java.util.Scanner
-
-fun main(){
+fun main() {
     val scanner = Scanner(System.`in`)
+    println("--- PERPUSTAKAAN UMN ---")
 
-    println("-----APLIKASI PMB UMN -----");
+    print("Judul Buku: ")
+    val bookTitle = scanner.nextLine()
 
-    print("Msukan Nama: ")
-    val name = scanner.nextLine()
+    print("Peminjam: ")
+    val borrower = scanner.nextLine()
 
-    print("Maikan NIM (Wajib 5 Karakter): ")
-    val nim = scanner.next()
+    print("Lama Pinjam (Hari): ")
+    var days = scanner.nextInt()
     scanner.nextLine()
 
-    if (nim.length != 5) {
-        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
-    } else {
-        // --- LOGIKA BARU DIMULAI DARI SINI ---
-        print("Pilih Jalur (1. Reguler, 2. Umum): ")
-        val type = scanner.nextInt()
-        scanner.nextLine() // PENTING: Consume newline setelah nextInt()!
 
-        if (type == 1) {
-            // Jalur 1: User input jurusan manual (Pakai Primary Constructor)
-            print("Masukkan Jurusan: ")
-            val major = scanner.nextLine()
-
-            val s1 = Student(name, nim, major)
-            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
-
-        } else if (type == 2) {
-            // Jalur 2: Jurusan otomatis (Pakai Secondary Constructor)
-            val s2 = Student(name, nim)
-            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
-
-        } else {
-            println("Pilihan ngawur, pendaftaran batal!")
-        }
+    if (days < 0) {
+        println("WARNING: Input hari minus! Diubah otomatis jadi 1 hari.")
+        days = 1
     }
+
+
+    val loan = Loan(bookTitle, borrower, days)
+
+
+    println("\n--- DETAIL PEMINJAMAN ---")
+    println("Buku: ${loan.bookTitle}")
+    println("Peminjam: ${loan.borrower}")
+    println("Durasi: ${loan.loanDuration} hari")
+    println("Total Denda: Rp ${loan.calculateFine()}")
 
 }
