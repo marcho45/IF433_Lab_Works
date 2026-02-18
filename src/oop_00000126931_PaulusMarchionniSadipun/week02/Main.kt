4489d76bf70d2d78e96d21f2532b8d51ff54ce62
marcho45 <paulusmarchionni@gmail.com>
2026-02-18 15:24:02 +0700

    week03: create basic Employee class

--- src/oop_00000126931_PaulusMarchionniSadipun/week02/Main.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week02/Main.kt
@@ -1,34 +1,63 @@
 package oop_00000126931_PaulusMarchionniSadipun.week02
 
 import java.util.Scanner
+
 fun main() {
     val scanner = Scanner(System.`in`)
-    println("--- PERPUSTAKAAN UMN ---")
-
-    print("Judul Buku: ")
-    val bookTitle = scanner.nextLine()
-
-    print("Peminjam: ")
-    val borrower = scanner.nextLine()
-
-    print("Lama Pinjam (Hari): ")
-    var days = scanner.nextInt()
-    scanner.nextLine()
+    println("--- RPG BATTLE ARENA ---")
 
 
-    if (days < 0) {
-        println("WARNING: Input hari minus! Diubah otomatis jadi 1 hari.")
-        days = 1
+    print("Masukkan Nama Hero: ")
+    val heroName = scanner.nextLine()
+
+    print("Masukkan Base Damage Hero (misal 15): ")
+    val heroDamage = scanner.nextInt()
+
+
+    val myHero = Hero(heroName, baseDamage = heroDamage)
+
+
+    var enemyHp = 100
+    println("Musuh muncul dengan HP: $enemyHp")
+    println("PERTARUNGAN DIMULAI!")
+
+
+    while (myHero.isAlive() && enemyHp > 0) {
+        println("\n--- GILIRANMU ---")
+        println("1. Serang")
+        println("2. Kabur")
+        print("Pilihan: ")
+        val action = scanner.nextInt()
+
+        if (action == 1) {
+
+            myHero.attack("Musuh Jahat")
+            enemyHp -= myHero.baseDamage
+            if (enemyHp < 0) enemyHp = 0
+            println("Musuh terkena ${myHero.baseDamage} damage. Sisa HP Musuh: $enemyHp")
+
+
+            if (enemyHp > 0) {
+                println("\n--- GILIRAN MUSUH ---")
+
+                val enemyDamage = (10..20).random()
+                myHero.takeDamage(enemyDamage)
+            }
+        } else if (action == 2) {
+            println("${myHero.name} melarikan diri dari pertarungan...")
+            break
+        } else {
+            println("Aksi tidak valid! Kehilangan giliran.")
+        }
     }
 
 
-    val loan = Loan(bookTitle, borrower, days)
-
-
-    println("\n--- DETAIL PEMINJAMAN ---")
-    println("Buku: ${loan.bookTitle}")
-    println("Peminjam: ${loan.borrower}")
-    println("Durasi: ${loan.loanDuration} hari")
-    println("Total Denda: Rp ${loan.calculateFine()}")
-
+    println("\n--- HASIL AKHIR ---")
+    if (myHero.isAlive() && enemyHp <= 0) {
+        println("SELAMAT! ${myHero.name} MENANG!")
+    } else if (!myHero.isAlive()) {
+        println("GAME OVER. ${myHero.name} gugur dalam pertarungan.")
+    } else {
+        println("Pertarungan berakhir imbang/kabur.")
+    }
 }
---
+++ src/oop_00000126931_PaulusMarchionniSadipun/week03/Employee.kt
@@ -0,0 +1,6 @@
+package oop_00000126931_PaulusMarchionniSadipun.week03
+
+class Employee(val name: String) {
+    var salary: Int = 0
+}
+
