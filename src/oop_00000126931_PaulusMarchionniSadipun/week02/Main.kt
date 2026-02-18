17807e5afeab2e2a140b14e5819b1669d2bcb3bb
marcho45 <paulusmarchionni@gmail.com>
2026-02-18 10:40:41 +0700

    week02: implement selection logic for constructors

--- src/oop_00000126931_PaulusMarchionniSadipun/week02/Main.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week02/Main.kt
@@ -12,18 +12,32 @@ fun main(){
 
     print("Maikan NIM (Wajib 5 Karakter): ")
     val nim = scanner.next()
-
     scanner.nextLine()
 
     if (nim.length != 5) {
         println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
     } else {
-        print("Masukkan Jurusan: ")
-        val major = scanner.nextLine()
+        // --- LOGIKA BARU DIMULAI DARI SINI ---
+        print("Pilih Jalur (1. Reguler, 2. Umum): ")
+        val type = scanner.nextInt()
+        scanner.nextLine() // PENTING: Consume newline setelah nextInt()!
 
-        // Instansiasi Objek karena data sudah aman
-        val s1 = Student(name, nim, major)
-        println("Status: Pendaftaran Selesai.")
+        if (type == 1) {
+            // Jalur 1: User input jurusan manual (Pakai Primary Constructor)
+            print("Masukkan Jurusan: ")
+            val major = scanner.nextLine()
+
+            val s1 = Student(name, nim, major)
+            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+
+        } else if (type == 2) {
+            // Jalur 2: Jurusan otomatis (Pakai Secondary Constructor)
+            val s2 = Student(name, nim)
+            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+
+        } else {
+            println("Pilihan ngawur, pendaftaran batal!")
+        }
     }
 
 }
