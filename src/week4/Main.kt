f775c5f592a92d3d545b1ad34a476732db2d4c26
marcho45 <paulusmarchionni@gmail.com>
2026-02-27 02:07:50 +0700

    week04: test Vehicle and Car in main

---
+++ src/week4/Main.kt
@@ -0,0 +1,16 @@
+package week4
+
+import oop_00000126931_PaulusMarchionniSadipun.week04.Vehicle
+
+fun main () {
+    println("--- Testing Vehicle ---");
+    val generalVehicle = Vehicle(brand = "Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car(brand = "Toyota", numberOfDoors = 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
