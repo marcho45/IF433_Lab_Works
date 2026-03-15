85cedaa1c9315447f19e764319c13842cd1eba0a
marcho45 <paulusmarchionni@gmail.com>
2026-03-15 19:02:03 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000126931_PaulusMarchionniSadipun/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000126931_PaulusMarchionniSadipun.week06
+
+interface Camera {
+    fun turnOn() {
+        println("Lensa camera terbuka dan sensor aktif!!")
+    }
+
+    interface Phone {
+        fun turnOn() {
+            println("sinyal seluler mencari jaringan")
+        }
+    }
+}
