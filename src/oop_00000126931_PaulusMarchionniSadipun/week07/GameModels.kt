6113df2ce1d24692102b14c907b3d4597433c7bd
marcho45 <paulusmarchionni@gmail.com>
2026-03-25 16:25:33 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000126931_PaulusMarchionniSadipun/week07/GameModels.kt
@@ -0,0 +1,11 @@
+package oop_00000126931_PaulusMarchionniSadipun.week07
+
+enum class GameModels (val dropChance: Int) {
+
+    COMMON (70),
+    UNCOMMON(50),
+    RARE(35),
+    EPIC(10),
+    LEGENDARY(1)
+
+}
