a9a58051d7d66c16123944c91626c73c46f2c1d7
marcho45 <paulusmarchionni@gmail.com>
2026-03-25 16:30:37 +0700

    week07: (task) create GameItem data class

--- src/oop_00000126931_PaulusMarchionniSadipun/week07/GameModels.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week07/GameModels.kt
@@ -1,11 +1,17 @@
 package oop_00000126931_PaulusMarchionniSadipun.week07
 
-enum class GameModels (val dropChance: Int) {
+import java.awt.event.ItemEvent
+
+enum class ItemRarity (val dropChance: Int) {
 
     COMMON (70),
     UNCOMMON(50),
     RARE(35),
     EPIC(10),
     LEGENDARY(1)
+
+}
+
+data class GameItem(val name: String, val damage: Int, val rarity: ItemRarity) {
 
 }
