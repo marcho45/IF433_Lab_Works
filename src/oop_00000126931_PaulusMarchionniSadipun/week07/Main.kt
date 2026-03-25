512c895cf80d46508af8ee69025f05b836085e0d
marcho45 <paulusmarchionni@gmail.com>
2026-03-25 16:10:56 +0700

    week07: implement copy and destructuring on data class

--- src/oop_00000126931_PaulusMarchionniSadipun/week07/Main.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week07/Main.kt
@@ -15,4 +15,10 @@ fun main () {
     println(data1)
     println("Sama? ${data1 == data2}")
 
+    val data3 = data1.copy(age = 23)
+    println("Hasil copy: $data3")
+
+    val (userName, userAge) = data1
+    println("Destructured: $userName berumur $userAge")
+
 }
