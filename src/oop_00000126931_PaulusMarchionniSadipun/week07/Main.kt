9431c30f9fb6b51d1e6dcfab0054b717a44c33f5
marcho45 <paulusmarchionni@gmail.com>
2026-04-09 22:48:56 +0700

    bnenny updatye

--- src/oop_00000126931_PaulusMarchionniSadipun/week07/Main.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week07/Main.kt
@@ -27,7 +27,7 @@ fun main () {
     val uiMessage = when(response) {
         is ApiResponse.Success -> "Tampilkan: ${response.data}"
         is ApiResponse.Error -> "Mulcul Alert: ${response.massage}"
-        is ApiResponse.Loading -> "Tampilkan Spinner"
+        is ApiResponse.Loading -> "Tampilkan Spinner benny"
     }
 
     GameManager.startGame()
