775258fbdc03f9c37f3a5ddabfa7b9cbb2df9d71
marcho45 <paulusmarchionni@gmail.com>
2026-04-29 15:25:54 +0700

    week08: create basic UserProfile with nullable fields

---
+++ src/oop_00000126931_PaulusMarchionniSadipun/week8/UserProfile.kt
@@ -0,0 +1,9 @@
+package oop_00000126931_PaulusMarchionniSadipun.week8
+
+import oop_00000126931_PaulusMarchionniSadipun.week06.Phone
+
+class UserProfile (
+    val name: String,
+    val email: String?,
+    val phone: String? = null
+)
