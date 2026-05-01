aa786b17e688ed818db1c29e74a700d50a8619cd
marcho45 <paulusmarchionni@gmail.com>
2026-05-01 18:43:14 +0700

    week08: (task) process mixed raw JSON list using as?

--- src/oop_00000126931_PaulusMarchionniSadipun/week8/ApiParser.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week8/ApiParser.kt
@@ -23,4 +23,19 @@ class ApiParser {
             else -> null
         }
     }
+
+    fun checkout(product: Product) {
+        // Ekstrak ID dari product dengan pola when
+        val id = when (product) {
+            is Product.Electronic -> product.id
+            is Product.Clothing -> product.id
+        }
+
+        // Lempar ID ke Java Service
+        val rawResponse = JavaPaymentService.processPayment(id)
+
+        // Gunakan !! untuk membuktikan Java Interop
+        val trxId = rawResponse!!
+        println("Berhasil Checkout! Transaction ID: $trxId")
+    }
 }
