9a282f0ae3f1b8b2870f2b1c6a68b14de73324c8
marcho45 <paulusmarchionni@gmail.com>
2026-05-01 18:29:14 +0700

    week08: create fallback logic with as? and elvis

--- src/oop_00000126931_PaulusMarchionniSadipun/week8/Main.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week8/Main.kt
@@ -39,4 +39,9 @@ fun main() {
         }
     }
 
+    val someObject: Any = 100
+
+    val safeString = someObject as? String ?: "Unknown String"
+    println("Hasil cast fallback: $safeString")
+
 }
