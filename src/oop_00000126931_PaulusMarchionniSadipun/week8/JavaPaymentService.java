19bcd93f8dd288e75aa9e814e06894c0dcd45040
marcho45 <paulusmarchionni@gmail.com>
2026-05-01 18:41:50 +0700

    week08: (task) create Java Payment Service for payment interop

---
+++ src/oop_00000126931_PaulusMarchionniSadipun/week8/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000126931_PaulusMarchionniSadipun.week8;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
