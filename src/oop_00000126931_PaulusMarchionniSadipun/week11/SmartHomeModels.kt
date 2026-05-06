6210f2a40dd55aa1535bdd9a8dcb3d1a9c54a843
marcho45 <paulusmarchionni@gmail.com>
2026-05-06 16:00:39 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000126931_PaulusMarchionniSadipun/week11/SmartHomeModels.kt
@@ -0,0 +1,9 @@
+package oop_00000126931_PaulusMarchionniSadipun.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
+
