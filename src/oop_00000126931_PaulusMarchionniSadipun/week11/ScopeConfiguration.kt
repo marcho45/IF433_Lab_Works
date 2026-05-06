8d675dea570b10e60307c85c09a654c60eda69c3
marcho45 <paulusmarchionni@gmail.com>
2026-05-06 15:44:52 +0700

    week11: implement apply for object initialization

--- src/oop_00000126931_PaulusMarchionniSadipun/week11/ScopeConfiguration.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week11/ScopeConfiguration.kt
@@ -1,3 +1,11 @@
 package oop_00000126931_PaulusMarchionniSadipun.week11
 
 data class User(var name: String = "", var age: Int = 0)
+
+fun main() {
+    println("=== TEST APPLY ===")
+    val user = User().apply {
+        name = "Alex"
+        age = 25 // Typo modul $age=25$ diperbaiki
+    }
+    println(user)
