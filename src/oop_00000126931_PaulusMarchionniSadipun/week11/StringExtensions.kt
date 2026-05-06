41b23b57f7f1733dbc4ccbfa52aa97632f041aaa
marcho45 <paulusmarchionni@gmail.com>
2026-05-06 15:24:35 +0700

    week11: create string extension functions

---
+++ src/oop_00000126931_PaulusMarchionniSadipun/week11/StringExtensions.kt
@@ -0,0 +1,9 @@
+package oop_00000126931_PaulusMarchionniSadipun.week11
+
+fun String.addGreeting() : String {
+    return "Hello $this"
+}
+
+fun String.repeatlines(n: Int) : String {
+    return this.repeat(n)
+}
