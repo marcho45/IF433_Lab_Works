93398eb0d514dd0e419c59ab01f16bc9f1b3a4be
marcho45 <paulusmarchionni@gmail.com>
2026-05-06 15:57:46 +0700

    week11: combine apply and also in object setup

--- src/oop_00000126931_PaulusMarchionniSadipun/week11/ScopeConfiguration.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week11/ScopeConfiguration.kt
@@ -23,3 +23,11 @@ fun main() {
     with(user) {
         println("User Detail -> Nama: $name, Umur: $age")
     }
+
+    println("\n=== GABUNGAN APPLY & ALSO ===")
+    val newUser = User("Budi", 20).apply {
+        age = 21 // Mengubah umur
+    }.also {
+        println("User baru berhasil dibuat: $it")
+    }
+}
