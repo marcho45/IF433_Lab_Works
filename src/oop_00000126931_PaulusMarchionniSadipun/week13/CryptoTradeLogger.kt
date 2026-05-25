db85db7179794248acb117f59519b0d99c4b0ab0
Marcho <paulusmarchionni@gmail.com>
2026-05-25 17:11:20 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_00000126931_PaulusMarchionniSadipun/week13/CryptoTradeLogger.kt
@@ -0,0 +1,8 @@
+package oop_00000126931_PaulusMarchionniSadipun.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class TradeRecord(val id: Int, val symbol: String, val type: String, val margin: Double, val pnl: Double)
+
+fun TradeRecord.toCsv(): String = "$id,$symbol,$type,$margin,$pnl"
