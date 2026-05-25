0811989bf9cc87103dade468af35bb49daec7738
Marcho <paulusmarchionni@gmail.com>
2026-05-25 17:00:09 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_00000126931_PaulusMarchionniSadipun/week12/SmartFeeder.kt
@@ -0,0 +1,8 @@
+package oop_00000126931_PaulusMarchionniSadipun.week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+
+    if (isJammed) {
+        throw DispenserJamException()
+    }
