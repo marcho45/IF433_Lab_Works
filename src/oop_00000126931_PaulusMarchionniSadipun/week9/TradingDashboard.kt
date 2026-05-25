9ca3724ab4273242c50a656c44d6d4b7b934f72f
Marcho <paulusmarchionni@gmail.com>
2026-05-25 16:35:25 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000126931_PaulusMarchionniSadipun/week9/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_00000126931_PaulusMarchionniSadipun.week9
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 50, -12.0, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", 5, 2.5, "OPEN"),
+        TradeLog("ETHUSDT", "LONG", 15, 8.4, "CLOSED"),
+        TradeLog("SOLUSDT", "SHORT", 10, -2.1, "OPEN")
+    )
+}
