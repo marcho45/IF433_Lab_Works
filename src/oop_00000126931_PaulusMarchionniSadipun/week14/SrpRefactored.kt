fcea5ff3946aa49240d51e49641784b2d28ddd48
Marcho <paulusmarchionni@gmail.com>
2026-05-25 17:20:22 +0700

    week14: extract EmailService to fix SRP

--- src/oop_00000126931_PaulusMarchionniSadipun/week14/SrpRefactored.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week14/SrpRefactored.kt
@@ -1,2 +1,17 @@
 package oop_00000126931_PaulusMarchionniSadipun.week14
 
+class UserValidator {
+    fun validate(user: User): Boolean = user.email.contains("@") && user.age >= 18
+}
+
+class UserRepository {
+    fun save(user: User) {
+        println("Saving user: ${user.name} to Database")
+    }
+}
+
+class EmailService {
+    fun sendWelcome(user: User) {
+        println("Welcome email -> ${user.email}")
+    }
+}
