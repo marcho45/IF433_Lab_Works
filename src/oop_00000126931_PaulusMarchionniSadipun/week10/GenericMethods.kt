8c372f05ef6f6bb91c38c4b58d64105a80e80597
Marcho <paulusmarchionni@gmail.com>
2026-05-25 16:48:56 +0700

    week10: implement basic generic function printData

---
+++ src/oop_00000126931_PaulusMarchionniSadipun/week10/GenericMethods.kt
@@ -0,0 +1,9 @@
+package oop_00000126931_PaulusMarchionniSadipun.week10
+
+fun <T> printData(data: T) {
+    println("Data yang diterima: $data")
+}
+
+fun <T> processData(input: T): T {
+    return input
+}
