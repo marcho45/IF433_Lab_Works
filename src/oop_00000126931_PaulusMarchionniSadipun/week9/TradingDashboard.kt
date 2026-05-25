ecc416948c8839d5742f6f7a62b81a3c06b1c59c
Marcho <paulusmarchionni@gmail.com>
2026-05-25 16:37:05 +0700

    week09: (task) print top performers using forEach

--- src/oop_00000126931_PaulusMarchionniSadipun/week9/TradingDashboard.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week9/TradingDashboard.kt
@@ -22,4 +22,7 @@ fun main() {
     val uniquePairs = tradeHistory
         .map { it.pair }
         .toSet()
+
+    println("=== CRYPTO TRADING DASHBOARD ===")
+    topPerformersString.forEach { println(it) }
 }
