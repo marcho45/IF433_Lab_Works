c42be7909f365e45eba68432cbfa4c8b9e4e14e6
Marcho <paulusmarchionni@gmail.com>
2026-05-25 16:49:37 +0700

    week10: implement type constraint for numeric types

---
+++ src/oop_00000126931_PaulusMarchionniSadipun/week10/NumericalStorage.kt
@@ -0,0 +1,11 @@
+package oop_00000126931_PaulusMarchionniSadipun.week10
+
+class MathBox<T : Number>(val value1: T, val value2: T) {
+    fun sum(): Double {
+        return value1.toDouble() + value2.toDouble()
+    }
+}
+
+fun <T> getMax(a: T, b: T): T where T : Comparable<T> {
+    return if (a > b) a else b
+}
