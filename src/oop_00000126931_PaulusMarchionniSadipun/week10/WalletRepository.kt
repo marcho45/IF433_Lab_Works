a8a0b4070b5f4cd146e551c4098e54a5d5a54f2a
Marcho <paulusmarchionni@gmail.com>
2026-05-25 16:51:43 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_00000126931_PaulusMarchionniSadipun/week10/WalletRepository.kt
@@ -0,0 +1,13 @@
+package oop_00000126931_PaulusMarchionniSadipun.week10
+
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+}
