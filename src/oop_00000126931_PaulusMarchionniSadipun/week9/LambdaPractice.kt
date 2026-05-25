9da255e483cae5ec530f6e43237b7b4dc20296b9
Marcho <paulusmarchionni@gmail.com>
2026-05-25 16:32:12 +0700

    week09: implement filter HOF to extract data

--- src/oop_00000126931_PaulusMarchionniSadipun/week9/LambdaPractice.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week9/LambdaPractice.kt
@@ -2,6 +2,8 @@ package oop_00000126931_PaulusMarchionniSadipun.week9
 
 
 
+
+
 fun main() {
     println("=== TEST LAMBDA ===")
     val sumLambda = { a: Int, b: Int -> a + b }
@@ -9,10 +11,4 @@ fun main() {
 
     val squareImplicit: (Int) -> Int = { it * it }
     println("Hasil Square: ${squareImplicit(4)}")
-
-    println("\n=== HOF: MAP ===")
-    val multiplied = evens.map { it * 10 }
-    val asStrings = multiplied.map { "Rp ${it}k" }
-    println("Multiplied: $multiplied")
-    println("Formatted: $asStrings")
 }
