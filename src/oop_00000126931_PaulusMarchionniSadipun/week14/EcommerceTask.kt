e3698cd6b37bb3814389ece835b94f634b18bf1f
Marcho <paulusmarchionni@gmail.com>
2026-05-25 17:24:01 +0700

    week14: (task) copy bad E-Commerce Order Processor code

---
+++ src/oop_00000126931_PaulusMarchionniSadipun/week14/EcommerceTask.kt
@@ -0,0 +1,19 @@
+package oop_00000126931_PaulusMarchionniSadipun.week14
+
+import java.io.File
+import java.io.FileWriter // Kita import ini untuk persiapan pakai blok use nanti
+
+class BadOrderProcessor {
+    private val file = File("orders.csv")
+
+    fun processOrder(itemName: String, basePrice: Double, customerType: String) {
+        val finalPrice = when (customerType) {
+            "REGULAR" -> basePrice
+            "VIP" -> basePrice * 0.90
+            else -> basePrice
+        }
+        println("Memproses pesanan $itemName seharga $finalPrice")
+        file.appendText("$itemName, $finalPrice, $customerType\n")
+        println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
+    }
+}
