489dfdeaed0840e3c6cf00fdd4075c31b0687845
Marcho <paulusmarchionni@gmail.com>
2026-05-25 16:53:36 +0700

    week10: (task) populate coin repository

--- src/oop_00000126931_PaulusMarchionniSadipun/week10/CryptoDashboard.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_00000126931_PaulusMarchionniSadipun.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 4.2))
+    coinRepo.add(Coin("USDT", 1500.0))
 }
