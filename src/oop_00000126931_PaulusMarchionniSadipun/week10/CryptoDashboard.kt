c2519abcd145604263b514a74b4c791503f405e8
Marcho <paulusmarchionni@gmail.com>
2026-05-25 16:53:48 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000126931_PaulusMarchionniSadipun/week10/CryptoDashboard.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week10/CryptoDashboard.kt
@@ -5,4 +5,6 @@ fun main() {
     coinRepo.add(Coin("BTC", 0.5))
     coinRepo.add(Coin("ETH", 4.2))
     coinRepo.add(Coin("USDT", 1500.0))
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }
