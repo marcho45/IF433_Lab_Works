d794538828e8a141c9bcee2fe218ada6ad820e57
Marcho <paulusmarchionni@gmail.com>
2026-05-25 16:54:24 +0700

    week10: (task) initialize and populate transaction repository

--- src/oop_00000126931_PaulusMarchionniSadipun/week10/CryptoDashboard.kt
+++ src/oop_00000126931_PaulusMarchionniSadipun/week10/CryptoDashboard.kt
@@ -13,4 +13,13 @@ fun main() {
     response.data.forEach {
         println("Koin: ${it.name} | Saldo: ${it.balance}")
     }
+
+    println("\n=== TRANSACTION LOGS ===")
+    val txRepo = WalletRepository<Transaction>()
+    txRepo.add(Transaction("TX-991A", 0.15))
+    txRepo.add(Transaction("TX-992B", 1.2))
+
+    txRepo.getAll().forEach {
+        println("ID: ${it.id} | Jumlah: ${it.amount}")
+    }
 }
